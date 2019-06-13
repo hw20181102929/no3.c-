17f15c6d3509527f1b04178816e536259bf84baf
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-13 12:10:40 +0800

    no.3
    c++

--- no3.c++/main.cpp
+++ no3.c++/main.cpp
@@ -1,15 +1,78 @@
-//
-//  main.cpp
-//  no3.c++
-//
-//  Created by s20181102929 on 2019/6/13.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+template<class T>
+class MyArray{
+private:
+    int size;
+    T *data;
+public:
+    MyArray(int size);
+    ~MyArray();
+    bool check();
+    void sort();
+    void display();
+};
+template<class T>
+MyArray<T>::MyArray(int size):size(size){
+    data=new T[size];
+}
+template<class T>
+void MyArray<T>::sort(){
+    int i;
+    for(i=0;i<size;i++){
+        cin>>data[i];
+    }
+    T t=data[0];
+    for(i=0;i<size-1;i++){
+        for(int j=0;j<size-1;j++){
+            if(data[j]>data[j+1]){
+                t=data[j];
+                data[j]=data[j+1];
+                data[j+1]=t;
+            }
+        }
+    }
+}
+template<class T>
+void MyArray<T>::display(){
+    int i;
+    for(i=0;i<size;i++){
+        if(i<size-1)
+            cout<<data[i]<<" ";
+        else
+        {
+            cout<<data[i];
+        }
+    }
+    cout<<endl;
+}
+
+template<class T>
+MyArray<T>::~MyArray(){ delete[] data;}
+
+template<class T>
+bool MyArray<T>::check(){
+    int i;
+    for(i=0;i<size-1;i++)
+        if(data[i]>data[i+1]) { cout<<"ERROR!"<<endl;return false;}
+    return true;
+}
+int main( )
+{
+    MyArray<int> *pI;
+    MyArray<float> *pF;
+    MyArray<char> *pC;
+    int ty, size;
+    cin>>ty;
+    while(ty>0){
+        cin>>size;
+        switch(ty){
+            case 1: pI = new MyArray<int>(size);   pI->sort(); pI->check(); pI->display(); delete pI; break;
+            case 2: pF = new MyArray<float>(size); pF->sort(); pF->check(); pF->display(); delete pF; break;
+            case 3: pC = new MyArray<char>(size);  pC->sort(); pC->check(); pC->display(); delete pC; break;
+        }
+        cin>>ty;
+    }
     return 0;
 }
